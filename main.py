3c3770726ea6a6ce7f79e79bfa3e9896bab73a15
Joshua Hecker <jhecker2001@gmail.com>
2023-12-09 12:58:51 -0600

    Added home page.

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home_page():
+    html = ('<h1>Guess a Number between 0 and 9</h1>'
+            '<p>Enter your guess at the end of the url.</p>'
+            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" '
+            'alt="Gif of various numbers rapidly appearing"/>')
+    return html
+
+
 if __name__ == "__main__":
     app.run(debug=True)
